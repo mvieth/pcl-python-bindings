5c296216bd9c6da7d41d521677c1a0917e90513d
Lukas Huber <hubernikus@gmail.com>
2025-07-25 01:21:28 +0200

    Draft of features.

--- src/python_features.cpp
+++ src/python_features.cpp
@@ -1,3 +1,5 @@
+#include <variant>
+
 #include <nanobind/nanobind.h>
 #include <nanobind/stl/string.h>
 #include <nanobind/stl/shared_ptr.h>
@@ -9,16 +11,57 @@
 #include "bind_utils.hpp"
 
 namespace nb = nanobind;
+using namespace nb::literals;
 
-struct NormalEstimation{
-  int k_search = 0;
-  double radius_search = 0.0;
+template<typename T>
+std::shared_ptr<T> create_shared_ptr(T& value){
+  std::shared_ptr<T> pointer(&value);
+  return pointer;
+};
+
+class NormalEstimation{
+  public:
+    int k_search;
+    double radius_search;
+
+    NormalEstimation(int k_search, double radius_search): k_search(k_search), radius_search(radius_search) {}
+};
+
+template<typename T>
+pcl::NormalEstimation<T, T> create_estimator(T&){
+  return pcl::NormalEstimation<T, T>();
+};
+
+template<typename T> requires (!HasPosition<T>)
+void compute_inplace(T& cloud, NormalEstimation&) {std::cout << "Does not have position.\n"; throw 101;};
+template<typename T> requires (!HasNormal<T>)
+void compute_inplace(T& cloud, NormalEstimation&) {std::cout << "Does not have normal.\n"; throw 101;};
+template<typename T> requires (HasPosition<T> && HasNormal<T>)
+void compute_inplace(T& cloud, const NormalEstimation& parameters){
+  // Pass point cloud to infer reference type
+  auto estimator = create_estimator(cloud.front());
+  estimator.setKSearch(parameters.k_search);
+  estimator.setRadiusSearch(parameters.radius_search);
+  auto cloud_ptr = create_shared_ptr(cloud);
+  estimator.setInputCloud(cloud_ptr);
+  estimator.compute(cloud);
 };
 
 NB_MODULE(pcl_features_ext, m)
 {
-  nb::class_<NormalEstimation>(m, "NormalEstimation", "NormalEstimation estimates local surface properties (surface normals and curvatures) at each 3D point.")
-  .def(nb::init<>(&add, "k_search"_a = 0, "radius_search"_a = 0.0))
+  nb::class_<NormalEstimation>(m, "NormalEstimation", 
+    "NormalEstimation estimates local surface properties (surface normals and curvatures) at each 3D point.")
+  .def(nb::init<int, double>(), "k_search"_a= 0, "radius_search"_a = 0.0)
+  .def_rw("k_search", &NormalEstimation::k_search, 
+    "Set the number of k nearest neighbors to use for the feature estimation.")
+  .def_rw("radius_search", &NormalEstimation::radius_search, 
+    "Set the sphere radius that is to be used for determining the nearest neighbors used for the feature estimation.")
+  .def("compute", [](NormalEstimation& estimation, PointCloud& cloud){
+    std::visit([&estimation](auto& arg){
+      compute_inplace(arg, estimation);
+    }, cloud.data);
+  },
+    "Estimate normals and curvatures and store them in the given output cloud.")
   ;
 
   // TODO what about NormalEstimationOMP? Maybe rather expose that one in Python?
--- tests/test_features.py
+++ tests/test_features.py
@@ -1,20 +1,44 @@
 # for pytest
-from pcl.common import PointcloudXYZNormal, PointXYZNormal
-from pcl.features import NormalEstimationXYZNormal
-from pcl.io import loadPCDFile
+from pcl.common import PointcloudXYZNormal, PointXYZNormal, PointCloud
+from pcl.common import PointType as t
+from pcl.features import NormalEstimationXYZNormal, NormalEstimation
 from math import isnan
+
+import numpy as np
+
 
 def test_normalestimation():
     cloud_normals = PointcloudXYZNormal()
-    #loadPCDFile("/home/markus/pcd_files/table_scene_mug_stereo_textured.pcd", cloud_normals)
+    # loadPCDFile("/home/markus/pcd_files/table_scene_mug_stereo_textured.pcd", cloud_normals)
     for x in range(15):
         for y in range(15):
             cloud_normals.append(PointXYZNormal(x, y, 0.0))
-    assert len(cloud_normals)>0
+    assert len(cloud_normals) > 0
     ne = NormalEstimationXYZNormal()
     ne.setInputCloud(cloud_normals)
     ne.setKSearch(50)
     ne.compute(cloud_normals)
     for point in cloud_normals:
         if not isnan(point.x):
-            assert abs(point.normal_x**2+point.normal_y**2+point.normal_z**2-1.0)<1e-5
+            assert (
+                abs(point.normal_x**2 + point.normal_y**2 + point.normal_z**2 - 1.0)
+                < 1e-5
+            )
+
+
+def test_normal_estimation_general():
+    cloud_normals = PointCloud(t.PointNormal)
+    num = 15
+    cloud_normals["position"] = np.hstack(
+        (np.arange(num), np.arange(num), np.zeros(num))
+    )
+    assert len(cloud_normals) == num
+
+    NormalEstimation(k_search=50, radius_search=0.0).compute(cloud_normals)
+
+    norm = np.sum(cloud_normals["normal"] ** 2, axis=1)
+    np.testing.assert_allclose(norm, np.ones(num))
+
+
+if __name__ == "__main__":
+    test_normal_estimation_general()
